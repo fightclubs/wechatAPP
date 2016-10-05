986f22ab0871e2b87765712589117f8f779d154c
luyang00 <393780933@qq.com>
2016-10-05 12:01:53 +0800

    after submit could auot-clean inputdata

--- pages/dialog/dialog.js
+++ pages/dialog/dialog.js
@@ -1,23 +1,27 @@
-var inputValue='';
 var chatfunction = require('function.js')
+
+
+
 Page({
-  data: {
-    focus: false,
-    inputValue: ''
+   data:{
+    inputText: '',
   },
-  bindKeyInput: function(e) {
-    this.setData({
-      inputValue: e.detail.value
-    })
+
+  /**
+   * 监听手机号输入
+   */
+  listenerPhoneInput: function(e) {
+      this.data.inputText = e.detail.value;
+     // return 'sb'
   },
   formSubmit: function(e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    console.log('form发生了submit事件，携带数据为：', this.data.inputText)
     wx.request({
         url: 'https://owlwang.com/wechat/test.php',
         method: 'POST',
         data: {
-          name: 'haha' ,
-          input: e.detail.value,
+          neme: 'haha' ,
+          input: this.data.inputText,
         },
         success: function(res) {
           console.log(res.data)
@@ -26,6 +30,7 @@ Page({
           console.log("request fail")
         }
     })
+    return 'asdsa'
   },
   onLoad: function () {
     chatfunction.loginDialog()
